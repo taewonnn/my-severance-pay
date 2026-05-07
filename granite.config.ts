f344b6c22265bba39c6f4e3b17087ac6f5fb6c66
taewonnn <thisis_tw94@naver.com>
2026-05-07 17:01:26 +0900

    fix

--- granite.config.ts
+++ granite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from '@apps-in-toss/web-framework/config';
 export default defineConfig({
   appName: 'my-severance-pay',
   brand: {
-    displayName: '내 퇴직금 얼마?',
-    primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: '/logo.png',
+    displayName: '내 퇴직금은 얼마?',
+    primaryColor: '#3182F6',
+    icon: 'https://static.toss.im/appsintoss/33837/cead943b-2d73-4794-b3dd-8c581420e9c2.png',
   },
   web: {
     host: 'localhost',
